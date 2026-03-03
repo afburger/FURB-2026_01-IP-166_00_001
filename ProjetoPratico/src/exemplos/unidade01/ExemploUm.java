ed9e2049c56cb970b21ecc973091c391370c813f
André Felipe Bürger <andre@iburger.com.br>
2026-03-03 20:04:41 -0300

    feat: Exemplo unidade 03

---
+++ ProjetoPratico/src/exemplos/unidade01/ExemploUm.java
@@ -0,0 +1,28 @@
+package exemplos.unidade01;
+
+public class ExemploUm {
+
+    // Atributos da nossa clase
+    float numeroFloat = 10.5f;
+    double numeroDouble = 11.4d;
+    double numeroDoubleDois = 15.2;
+
+    byte numeroByte = 127;
+    short numeroShort = 32767;
+    int numeroInt = 10;
+    long numeroLong = 22000L;
+    long numeroLong2 = 44000;
+
+    char caractere = 'a';
+
+    boolean valorVerdadeiro = true;
+    boolean valorFalso = false;
+
+    String valorTexto = "Qualquer mensagem em texto";
+
+    final int valorConstante = 55;
+
+    int valor3 = valorConstante + numeroInt; 
+    
+    
+}
